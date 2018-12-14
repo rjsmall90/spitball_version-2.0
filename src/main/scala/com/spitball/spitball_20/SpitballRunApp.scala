6324c16dbd2b1b8852fa656517b91f0096fd5580
Admin <admin@Admins-Macintosh-3.local>
2018-12-14 13:58:46 -0500

    SpringBoot added

---
+++ src/main/scala/com/spitball/spitball_20/SpitballRunApp.scala
@@ -0,0 +1,14 @@
+package com.spitball.spitball_20
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+class SpitballRunApp
+
+  object SpitballRunApp extends App {
+    SpringApplication.run(classOf[SpitballRunApp]);
+  }
+
+
