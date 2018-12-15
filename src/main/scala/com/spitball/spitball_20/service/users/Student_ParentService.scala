6843d3699cd5f43613b91442bf2c4b14524bafbd
Admin <admin@Admins-Macintosh-3.local>
2018-12-15 10:47:20 -0500

    srvcs updtd

--- src/main/scala/com/spitball/spitball_20/service/users/Student_ParentService.scala
+++ src/main/scala/com/spitball/spitball_20/service/users/Student_ParentService.scala
@@ -1,5 +1,8 @@
 package com.spitball.spitball_20.service
 
+import java.util
+
+import com.spitball.spitball_20.model.users.Student_Parent
 import com.spitball.spitball_20.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -7,4 +10,20 @@ import org.springframework.stereotype.Service
 @Service
 class Student_ParentService(@Autowired private val userRepository: UserRepository) {
 
+  def getAllUsers(): util.List[Student_Parent] = {
+    userRepository.findAll
+  }
+
+  def getUsers(id: Long): Student_Parent = {
+    userRepository.findOne(id)
+  }
+
+  def createUsers(student_parent: Student_Parent): String = {
+    userRepository.save(student_parent)
+    student_parent.getEmail
+  }
+
+  def deleteUsers(student_parent: Student_Parent) = {
+    userRepository.delete(student_parent)
+  }
 }
--- src/main/scala/com/spitball/spitball_20/service/users/TeacherService.scala
+++ src/main/scala/com/spitball/spitball_20/service/users/TeacherService.scala
@@ -1,10 +1,30 @@
 package com.spitball.spitball_20.service.users
 
+import java.util
+
+import com.spitball.spitball_20.model.users.{Teacher, User}
 import com.spitball.spitball_20.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class TeacherService(@Autowired private val userRepository: UserRepository) {
+
+  def getAllTeachers(): util.List[Teacher] = {
+    userRepository.findAll
+  }
+
+  def getTeacher(id: Long): Teacher = {
+    userRepository.findOne(id)
+  }
+
+  def createTeacher(teacher: Teacher): String = {
+    userRepository.save(teacher)
+    teacher.getEmail
+  }
+
+  def deleteTeacher(teacher: Teacher) = {
+    userRepository.delete(teacher)
+  }
 
 }
